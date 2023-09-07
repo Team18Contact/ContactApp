2ce2ceb6f13f38ca05e0b3fd6f3eeb3bc6a3e7f4
PMETNT <temp19@naver.com>
2023-09-07 15:52:36 +0900

    [FEAT] 디테일 페이지 버튼, 문자 및 통화 구현 완료

--- app/src/main/java/com/example/contactapp/detail/DetailFragment.kt
+++ app/src/main/java/com/example/contactapp/detail/DetailFragment.kt
@@ -1,5 +1,7 @@
 package com.example.contactapp.detail
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -48,6 +50,25 @@ class DetailFragment : Fragment() {
         tvEmail.text = receiveData?.email
         tvLocale.text = receiveData?.locale
         tvAbility.text = receiveData?.ability
+
+        /**
+         *  디테일 페이지에서 버튼을 누르면 전화와 문자를 보내는 기능 추가입니다.
+         */
+
+        btnCallButton.setOnClickListener {
+            val phoneNumber = tvPhoneNumber.text
+            val callUriSwipedPerson = Uri.parse("tel:${phoneNumber}")
+            startActivity(Intent(Intent.ACTION_CALL, callUriSwipedPerson))
+        }
+
+        btnMessageButton.setOnClickListener {
+            val phoneNumber = tvPhoneNumber.text
+            val sendUriSwipedPerson = Uri.parse("smsto:${phoneNumber}")
+            startActivity(Intent(Intent.ACTION_SENDTO, sendUriSwipedPerson))
+
+        } // 디테일 페이지 버튼 기능 구현 완료
+
+
     }
 
     fun setData(contact: ContactModel) {
