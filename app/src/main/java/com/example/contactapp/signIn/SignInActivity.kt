fed7cf2c720141a44bdf41964b751cc5c88c3367
PMETNT <temp19@naver.com>
2023-09-10 12:36:41 +0900

    [UI, FEAT] 스플래시, 로그인 배경화면 움짤 적용, 이름과 암호명으로 로그인

--- app/src/main/java/com/example/contactapp/signIn/SignInActivity.kt
+++ app/src/main/java/com/example/contactapp/signIn/SignInActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.contactapp.R
 import com.example.contactapp.main.MainActivity
 import com.example.contactapp.signUp.SignUpActivity
@@ -22,6 +24,11 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
+        /*Glide 배경 움짤 추가 */
+        val loginGlide = findViewById<ImageView>(R.id.login_glide)
+        Glide.with(this).load(R.raw.login_background).into(loginGlide)
+        //
+
         editTextEmailAddress = findViewById(R.id.et_login_id)
         editTextPassword = findViewById(R.id.et_login_password)
         buttonLogin = findViewById(R.id.buttonLogin)
@@ -29,7 +36,7 @@ class SignInActivity : AppCompatActivity() {
 
         buttonLogin.setOnClickListener {
 
-            val emailaddress = editTextEmailAddress.text.toString()
+            val login_name = editTextEmailAddress.text.toString()   // 이름, 암호명으로 로그인 변경 작업
             val password = editTextPassword.text.toString()
 
             val checkName = intent.getStringExtra("userName") ?: "name"
@@ -41,11 +48,11 @@ class SignInActivity : AppCompatActivity() {
             val checkImage = intent.getIntExtra("userImage", 0)
 
 
-            if (emailaddress.isEmpty() || password.isEmpty()) {
+            if (login_name.isEmpty() || password.isEmpty()) {
 
 
                 Toast.makeText(this, "아이디/비밀번호 둘 중 하나가 입력이 비어있습니다.", Toast.LENGTH_SHORT).show()
-            } else if((emailaddress.equals(checkEmailAddress)) && (password.equals(checkPw))){
+            } else if((login_name.equals(checkName)) && (password.equals(checkLocale))){
 
                 Toast.makeText(this, R.string.successLogin, Toast.LENGTH_SHORT).show()
 
---
+++ app/src/main/java/com/example/contactapp/signIn/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.example.contactapp.signIn
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.example.contactapp.R
+import com.example.contactapp.databinding.ActivitySplashBinding
+import com.example.contactapp.signUp.SignUpActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val splashImage = binding.imageSplash
+
+        Glide.with(this).load(R.raw.splash_background).into(splashImage)
+
+        splashImage.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
+
+
+
+
+
+
+    }
+}
